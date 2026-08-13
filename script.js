c25d18f06d656c4df839f55c0c402ddfe0a34946
MIsmaeelQazi <ismaeelqazi30@gmail.com>
2026-08-13 13:21:46 +0000

    background

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 const DaQazi = document.getElementById("CanvasWorthyOfMe")
 const DaBrush = DaQazi.getContext("2D")
 
-function CanvasSize{
-    Canvas.width = window.innerWidth;
-    Canvas.height = window.innerHeight;
+function CanvasSize(){
+    DaQazi.width = window.innerWidth;
+    DaQazi.height = window.innerHeight;
 }
 
 CanvasSize()
 window.addEventListener("resize", CanvasSize);
 
+function background() {
+    DaBrush.fillStyle = "black";
+    DaBrush.fillRect(0, 0, DaQazi.width, DaQazi.height);
+}
 
+
+
+
+background();
 
 
 
