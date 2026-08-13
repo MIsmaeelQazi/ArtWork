fdbcd26235a70f255d99313f090baa9ca9d51112
MIsmaeelQazi <ismaeelqazi30@gmail.com>
2026-08-13 12:58:31 +0000

    canvas done

---
+++ script.js
@@ -0,0 +1,16 @@
+const DaQazi = document.getElementById("CanvasWorthyOfMe")
+const DaBrush = DaQazi.getContext("2D")
+
+function CanvasSize{
+    Canvas.width = window.innerWidth;
+    Canvas.height = window.innerHeight;
+}
+
+CanvasSize()
+window.addEventListener("resize", CanvasSize);
+
+
+
+
+
+
