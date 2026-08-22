2e8825843788aac41193d12157aa20b354ab5b38
MIsmaeelQazi <ismaeelqazi30@gmail.com>
2026-08-22 15:20:02 +0000

    twinkles

--- script.js
+++ script.js
@@ -81,7 +81,7 @@ function Stars(){
         if (star.y < 0) star.y = DaQazi.height;
         if (star.y > DaQazi.height) star.y = 0;
 
-        const twinkle = Math.sin(t + star.TwinkyTime) * 0.4 + 0.6;
+        const twinkle = (Math.sin(t + star.TwinkyTime) * 0.4 + 0.6)*star.twinkle;
         DaBrush.beginPath();
         DaBrush.arc(star.x, star.y, star.radius, 0, Math.PI * 2);
 
