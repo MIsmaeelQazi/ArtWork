10abc7b0bb674ab6749318ae7f82e71416b7924f
MIsmaeelQazi <ismaeelqazi30@gmail.com>
2026-09-06 11:55:54 +0000

    comma missed

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ for (let _ = 0; _ < stars; _++){
         vy: (Math.random() - 0.5) * 0.3,
         radius: Math.random() ** 3 * 2.5 + 0.2,
         TwinkyTime: Math.random() * Math.PI * 2,
-        Twinkle : Math.random() * 0.7 + 0.2
+        Twinkle : Math.random() * 0.7 + 0.2,
         color : Colors[Math.floor(Math.random()* Colors.length)]
         
     });
