764ac31fcd5df5d38feeb18e420e9c0002ca3c92
MIsmaeelQazi <ismaeelqazi30@gmail.com>
2026-09-06 12:45:44 +0000

    i missed a semi colon but nothing happened ??

--- script.js
+++ script.js
@@ -102,7 +102,10 @@ function Stars(){
     t += 0.02;
 
     DaBrush.fillStyle = "#05070f";
-    DaBrush.fillRect(0,0,DaQazi.width,DaQazi.height)
+    DaBrush.fillRect(0,0,DaQazi.width,DaQazi.height);
+
+
+    
     for (const star of allStars){
         star.x += star.vx;
         star.y += star.vy;
