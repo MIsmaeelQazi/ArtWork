027527ffa62b5395487514f268c9e091183100b5
MIsmaeelQazi <ismaeelqazi30@gmail.com>
2026-09-07 12:30:36 +0000

    more colors

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ window.addEventListener("resize", CanvasSize);
 
 const stars = 5500;
 //const distance = 120;
-const Colors = [[255,255,255],[210,230,255],[255,245,220],[200,220,255]]
+const Colors = [[255,255,255],[210,230,255],[255,245,220],[225,210,255],[255,225,100],[190,225,255],]
 function Galaxy(_){
     const center = DaQazi.height * 0.8 - (_/ DaQazi.width) * DaQazi.height * 0.8;
 
